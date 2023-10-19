c47f4ca2adda58f4e1b9d76d24ba578882f05f04
Keziah Reynoso <ada_hack208@hackademy.mx>
2023-10-19 02:36:58 -0600

    first version

--- EnfermerasCalculadora/settings.py
+++ EnfermerasCalculadora/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'EnfermerasCalculadora.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'templates'],
+        'DIRS': [BASE_DIR, 'EnfermerasCalculadora/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

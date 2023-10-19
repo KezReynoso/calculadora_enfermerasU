bbbadf366ba3c10cfe37226c4456b47db73e1b7b
Keziah Reynoso <ada_hack208@hackademy.mx>
2023-10-19 00:46:42 -0600

    first version

--- EnfermerasCalculadora/views.py
+++ EnfermerasCalculadora/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from calculadora.forms import CalculatorForm
+from .forms import CalculatorForm
 
 class Index(View):
     def get(self, request):
